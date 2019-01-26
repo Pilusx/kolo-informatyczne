4bd2a15cedda36d05203ccd8d483b617f3868b3b
pilusx <poteka@wp.pl>
2019-01-26 21:18:05 +0100

    FCANDY 4

--- files/09-Dijkstra/fcandy.cpp
+++ files/09-Dijkstra/fcandy.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
-int n, D, M, C, res = INT_MAX, nv;
+int n, D, M, C, nv, res = INT_MAX, dc;
 vector<int> k;
 vector<int> c;
 vector<bool> visited;
 vector<int> update[2];
+
+void visit(int nv) {
+    visited[nv] = true;
+    res = min(res, abs(2*nv - D));
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -25,24 +30,25 @@ int main() {
     }
     M = (D + C)/2;
     visited.resize(M+1, false);
-    visited[0] = true;
+    visit(0);
     
     for(int i = 1; i <= n; i++) { // O(CKN)
-        int dc = c[i];
-        for(int i = 0; i < 2; i++)
-            update[i].clear();
+        dc = c[i];
         
-        for(int i = 0; i + dc < visited.size(); i++)
-            if(visited[i] && !visited[i + dc]) 
-                update[0].push_back(i);
+        update[1].clear();
+        for(int nv = visited.size() - 1; nv - dc >= 0; nv--) {
+            if(visited[nv-dc] && !visited[nv]) {
+                visit(nv);
+                update[1].push_back(nv);
+            }
+        }
         
-        for(int j = 1; j <= k[i] && !update[(j+1)%2].empty(); j++) {
+        for(int j = 2; j <= k[i] && !update[(j+1)%2].empty(); j++) {
             update[j%2].clear();
             for(auto& v : update[(j+1)%2]) {
                 nv = v + dc;
                 if(nv < visited.size() && !visited[nv]) {
-                    visited[nv] = true;
-                    res = min(res, abs(2*nv - D));
+                    visit(nv);
                     update[j%2].push_back(nv);
                 }
             }
